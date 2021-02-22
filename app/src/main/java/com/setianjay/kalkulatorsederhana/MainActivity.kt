be9de3564c7a7c2273009b25622213b78be2ffdb
setianjay <hari.setiaji2000@gmail.com>
2021-02-22 12:32:32 +0700

    Kalkulator Sederhana

--- app/src/main/java/com/setianjay/kalkulatorsederhana/MainActivity.kt
+++ app/src/main/java/com/setianjay/kalkulatorsederhana/MainActivity.kt
@@ -2,10 +2,43 @@ package com.setianjay.kalkulatorsederhana
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var edit_value1: EditText
+    private lateinit var edit_value2: EditText
+    private lateinit var button_calculator: Button
+    private lateinit var text_result: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+        initListner()
+    }
+
+    // Initialize View
+    private fun initView(){
+        edit_value1 = findViewById(R.id.edit_value1)
+        edit_value2 = findViewById(R.id.edit_value2)
+        button_calculator = findViewById(R.id.button_calculator)
+        text_result = findViewById(R.id.text_result)
+    }
+
+    // Initialize event
+    private fun initListner(){
+        button_calculator.setOnClickListener {
+            val value1 = edit_value1.text.toString().toInt()
+            val value2 = edit_value2.text.toString().toInt()
+            text_result.text = countCalculate(value1,value2)
+
+        }
+    }
+
+    private fun countCalculate(value1: Int, value2: Int): String{
+        val result = value1 + value2
+        return result.toString()
     }
 }
