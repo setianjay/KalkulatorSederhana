c7819f1258c5312d0a44fa42aa694e74de390b19
setianjay <hari.setiaji2000@gmail.com>
2021-02-22 14:25:40 +0700

    Menambahkan fungsi untuk radio button

--- app/src/main/java/com/setianjay/kalkulatorsederhana/MainActivity.kt
+++ app/src/main/java/com/setianjay/kalkulatorsederhana/MainActivity.kt
@@ -2,16 +2,17 @@ package com.setianjay.kalkulatorsederhana
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var edit_value1: EditText
     private lateinit var edit_value2: EditText
+    private lateinit var radio_group_opeators: RadioGroup
+    private lateinit var radio_button: RadioButton
     private lateinit var button_calculator: Button
     private lateinit var text_result: TextView
+
+    private var OPERATOR: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +25,7 @@ class MainActivity : AppCompatActivity() {
     private fun initView(){
         edit_value1 = findViewById(R.id.edt_value1)
         edit_value2 = findViewById(R.id.edt_value2)
+        radio_group_opeators = findViewById(R.id.rg_operators)
         button_calculator = findViewById(R.id.btn_count)
         text_result = findViewById(R.id.txt_result)
     }
@@ -39,17 +41,32 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(applicationContext, "Input tidak boleh kosong", Toast.LENGTH_SHORT).show()
             }
         }
+
+        radio_group_opeators.setOnCheckedChangeListener { group, checkedId ->
+            radio_button = findViewById(group.checkedRadioButtonId)
+            OPERATOR = radio_button.text.toString()
+            text_result.text = "HASIL"
+        }
     }
 
     private fun inputValidate(): Boolean{
         if (edit_value1.text.isNullOrEmpty() || edit_value2.text.isNullOrEmpty()){
+            return false
+        }else if(OPERATOR.isNullOrEmpty()){
             return false
         }
         return true
     }
 
     private fun countCalculate(value1: Int, value2: Int): String{
-        val result = value1 + value2
+        var result = 0
+        when(OPERATOR){
+            "+" -> result = value1 + value2
+            "-" -> result = value1 - value2
+            "x" -> result = value1 * value2
+            ":" -> result = value1 / value2
+        }
+
         return result.toString()
     }
 }
